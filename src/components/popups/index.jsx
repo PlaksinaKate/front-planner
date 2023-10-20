c5e8d85452eca0bcbbccb95d843624a05025c8e6
eplaksina <eplaksina@angelsit.ru>
2023-10-20 07:46:34 +0700

    fix: login dir name

--- src/components/popups/index.jsx
+++ src/components/popups/index.jsx
@@ -1,10 +1,10 @@
 import { Authentication } from "./authentication"
 import { Error } from "./error"
-import { Login } from "./Login"
 import { Registration } from "./registration"
 import { EventPopup } from "./event"
 import { JoinEventSuccess } from "./joinEventSuccess"
 import { LeaveEvent } from "./leaveEvent"
 import { CreateEvent } from "./createEvent"
+import { Login } from "./login"
 
 export { Authentication, Error, Login, Registration, EventPopup, JoinEventSuccess, LeaveEvent, CreateEvent }
